519967f4038ec1ece832f852ab85032a585e205b
Chris Cooper <chris@cooper.info>
2022-01-14 20:59:48 +1100

    Load world file specified from command line

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 
 int init(struct Renderer **renderer, struct World **world);
 void cleanup(struct Renderer *renderer, struct World *world);
+void printUsage();
 
 // Global GLFW window
 extern struct Window window;
@@ -14,9 +15,27 @@ int main(int argc, char *argv[]) {
 
     fprintf(stderr, "main: \n");
 
+    char *world_file_path = NULL;
+    int load_world_file = 0;
+    if (argc == 2) {
+        load_world_file = 1;
+        world_file_path = argv[1];
+        fprintf(stderr, "world file path = %s\n", world_file_path);
+    } else if (argc > 2) {
+        fprintf(stderr, "To many command line arguments. \n");
+        printUsage();
+        return 1;
+    }
+
     struct Renderer *renderer;
     struct World *world;
     if (!init(&renderer, &world)) {
+        cleanup(renderer, world);
+        return 1;
+    }
+
+    if (load_world_file && !worldLoadFromFile(world, world_file_path)) {
+        fprintf(stderr, "Failed to load world file %s\n", world_file_path);
         cleanup(renderer, world);
         return 1;
     }
@@ -55,4 +74,9 @@ void cleanup(struct Renderer *renderer, struct World *world) {
     worldDestroy(world);
     rendererDestroy(renderer);
     windowCleanup();
+}
+
+void printUsage() {
+    fprintf(stderr, "Call with one argument, the relative path to a world file.\n");
+    fprintf(stderr, "Or call with no arguments for an empty world file.\n");
 }
